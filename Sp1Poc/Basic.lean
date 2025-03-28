2b30d9097d7118a21b421deb31e6899c38fee143
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-03-28 09:19:00 +0100

    Cleanup proofs: - show primality explicitly (mostly as 'documentation') - simplify the proof it has no zero divisors - simplify the main proof

--- Sp1Poc/Basic.lean
+++ Sp1Poc/Basic.lean
@@ -1,27 +1,13 @@
 import Mathlib
 
 abbrev BabyBearPrime : ℕ := 2013265921
+lemma prime_BabyBearPrime : Nat.Prime BabyBearPrime := by norm_num
+
 abbrev BabyBear : Type := Fin BabyBearPrime
+
 instance : NeZero BabyBearPrime := by constructor; decide
 
 instance : NoZeroDivisors BabyBear := by
-  constructor; intros a b Heq; rw [Fin.mul_def] at Heq; by_contra; simp at *
-  next Hneq =>
-    rw [@Fin.mk_eq_zero _ a] at Hneq; rw [@Fin.mk_eq_zero _ b] at Hneq
-    -- if sorry is replaced with norm_num, I get a '(kernel) deep recursion detected' error
-    have HPrime : Nat.Prime BabyBearPrime := by sorry
-    have Hcp_a : Nat.Coprime BabyBearPrime a.1 := by
-      apply Nat.coprime_of_lt_prime
-      · omega
-      · exact a.2
-      · assumption
-    have Hcp_b : Nat.Coprime BabyBearPrime b.1 := by
-      apply Nat.coprime_of_lt_prime
-      · omega
-      · exact b.2
-      · assumption
-    have Hncp_ab : ¬ Nat.Coprime BabyBearPrime (a.1 * b.1) := by
-      rw [← Nat.Prime.dvd_iff_not_coprime HPrime];
-      omega
-    apply Hncp_ab
-    apply Nat.Coprime.mul_right <;> assumption
+  have : IsDomain (ZMod BabyBearPrime) := ZMod.instIsDomain (hp := ⟨prime_BabyBearPrime⟩)
+  simp [ZMod] at this
+  infer_instance
