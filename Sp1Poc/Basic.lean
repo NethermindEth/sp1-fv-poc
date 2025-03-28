8bfdbd9d21611a782d37750f7314c3716d41ee98
Petar Maksimovic <petar.maksimovic@runtimeverification.com>
2025-03-28 08:04:43 +0000

    better readability

--- Sp1Poc/Basic.lean
+++ Sp1Poc/Basic.lean
@@ -9,11 +9,10 @@ instance : NeZero BabyBearPrime := by constructor; decide
 instance : NoZeroDivisors BabyBear := by
   constructor; intros a b Heq; rw [Fin.mul_def] at Heq; by_contra Hneq; simp at *
   rw [@Fin.mk_eq_zero _ a] at Hneq; rw [@Fin.mk_eq_zero _ b] at Hneq
-  have Hcp_a : Nat.Coprime BabyBearPrime a.1 := by
-    apply Nat.coprime_of_lt_prime <;> [ omega; exact a.2; exact BabyBearPrimeIsPrime ]
-  have Hcp_b : Nat.Coprime BabyBearPrime b.1 := by
-    apply Nat.coprime_of_lt_prime <;> [ omega; exact b.2; exact BabyBearPrimeIsPrime ]
   have Hncp_ab : ¬ Nat.Coprime BabyBearPrime (a.1 * b.1) := by
     rw [← Nat.Prime.dvd_iff_not_coprime BabyBearPrimeIsPrime]; omega
   apply Hncp_ab
-  apply Nat.Coprime.mul_right <;> assumption
+  apply Nat.Coprime.mul_right <;> [
+    apply Nat.coprime_of_lt_prime <;> [ omega; exact a.2; exact BabyBearPrimeIsPrime ];
+    apply Nat.coprime_of_lt_prime <;> [ omega; exact b.2; exact BabyBearPrimeIsPrime ]
+  ]
