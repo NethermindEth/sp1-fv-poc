45e0194740ab1d79f6bf8e505ff7e939e97cde88
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-04-14 12:09:49 +0200

    Revert the Lean version and the sorry. - The CLI setup already uses a particular Lean version. Easier to revert this.

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open System Lake DSL
 
 package «sp1-poc» where version := v!"1.0.0"
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"v4.18.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"v4.17.0"
 
 private def v4_17_0 := "e7fd1a415c80985ade02a021172834ca2139b0ca"
 
